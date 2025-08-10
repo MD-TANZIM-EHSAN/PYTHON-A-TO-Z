70a76600efa23216d6356dd1d0099500952caa26
MD TANZIM EHSAN <mdtanzime@gmail.com>
2025-08-10 18:59:01 +0600

    Add files via upload

---
+++ 01_Comment.py
@@ -0,0 +1,8 @@
+# 1.Comments are notes in code 
+# 2.Python Skips comments 
+# 3.Comments make code:
+## Understable,readable,professional
+
+#This is a single line - comment
+
+X = 10 # store the value (This is in-line code)
