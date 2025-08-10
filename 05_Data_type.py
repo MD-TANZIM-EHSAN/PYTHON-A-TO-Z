c724feee6a9f65c087a76a092e7219c99e244fe0
MD TANZIM EHSAN <mdtanzime@gmail.com>
2025-08-11 00:25:26 +0600

    Add files via upload

---
+++ 05_Data_type.py
@@ -0,0 +1,15 @@
+# Data type 
+a =10       #int
+b =34.4     #float
+c ="Tanzim"  #string
+d ='Ehsan'   #string
+e ="1234"    #string
+f =True      #bool
+g =False     #bool
+h =None      #none-type
+i =""        #str=>blank
+j = " "      #str=>Empty
+
+# Each value has a data type 
+#It automatically detects  data type 
+#Dynamic : Data type can change anytime 
