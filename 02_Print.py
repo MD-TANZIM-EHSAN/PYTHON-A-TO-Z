afc0ea0509a4e77025b6a4da91788dda89286e01
MD TANZIM EHSAN <mdtanzime@gmail.com>
2025-08-10 18:59:16 +0600

    Add files via upload

---
+++ 02_Print.py
@@ -0,0 +1,19 @@
+
+#buit-in Python function that displays message on the output screen to communicate with users 
+print("Hi Python")
+
+#Escape Double Quote ==> Allows you to include a "  without ending it.
+print("Hi \"Python\"")
+
+#Escape Backslash(\\)
+#===> use Real blackslash \ not as start of an  escape sequence
+print("Path : c\\user\\data")
+
+#Escape New line 
+print("Message1\n")
+print("Message2")
+
+print("""Your Learning Path:
+\t-Python Basics
+\t-Data science
+\t-Python Basics""")
