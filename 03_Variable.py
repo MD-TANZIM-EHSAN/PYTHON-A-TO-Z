d952e3a08a100c2ee1f47563fefe225e4ed8dfb1
MD TANZIM EHSAN <mdtanzime@gmail.com>
2025-08-10 18:59:32 +0600

    Add files via upload

---
+++ 03_Variable.py
@@ -0,0 +1,12 @@
+# A Variable is name you create to store a value so you can use it later in your code.
+# make a program dynamic
+#Name to store a value 
+#Stored in memory 
+#Resusable anytime 
+#Updatable anytime
+
+name = "Tanzim"
+language ="python"
+print("Hi, My Name is", name)
+print("I want to learn", language)
+print(name, "wants to learn", language)
